65022f14505385cad07f7d1fc041e44129378b2a
Tobias Årud <me@tobiasarud.se>
2019-02-04 16:10:30 +0100

    projet page done. Tomorrow: Front-page and wp conversion.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import './sass/style.scss';
 
-const helloJs = "This is index.js!";
+document.addEventListener( 'DOMContentLoaded', () => {
+    const $navbarBurgers = Array.prototype.slice.call( document.querySelectorAll( '.navbar-burger' ), 0 );
 
-console.log(helloJs);
+    if ( $navbarBurgers.length > 0 ) {
+        $navbarBurgers.forEach( el => {
+            el.addEventListener( 'click', () => {
+                const target = el.dataset.target;
+                const $target = document.getElementById( target );
+
+                el.classList.toggle( 'is-active' );
+                $target.classList.toggle( 'is-active' );
+            } );
+        } );
+    }
+} );
