5409c95becc9c5de589bdb81355ee868c81d5b6c
Tobias Årud <me@tobiasarud.se>
2019-11-19 20:16:00 +0100

    added acf for links into header of single project

--- template-parts/content-singular.php
+++ template-parts/content-singular.php
@@ -3,9 +3,9 @@
         <div class="hero-body">
             <div class="container">
             <?php
-            if ( is_page() || is_singular( 'projects' ) ) :
+            if ( is_page() ){
                 the_title( '<h2 class="entry-title title">', '</h2>' );
-            elseif ( is_single() ) :
+            } elseif ( is_single() && !is_singular('projects') ){
                 the_title( '<h2 class="entry-title title">', '</h2>' );
                 ?>
                 <h3 class="entry-meta subtitle">
@@ -14,12 +14,24 @@
                     getPostAuthorHtml();
                     ?>
                 </h3><?php
-            endif;
+            } elseif ( is_singular( 'projects' ) ){ 
+                the_title( '<h2 class="entry-title title">', '</h2>' ); ?>
+                <div class="entry-meta buttons">
+                <?php
+                if( get_field('live_link') ) :
+                    echo '<a class="button is-primary is-inverted" href="' . esc_url( get_field('live_link') ) . '">'. __( 'Visit Website', 'eruma-roku' ) .'</a>';
+                endif;
+                if( get_field('github_link') ) :
+                    echo '<a class="button is-primary is-inverted" href="' . esc_url( get_field('github_link') ) . '"><span>'. __( 'Visit Github Repo', 'eruma-roku' ) .'</span><span class="icon is-medium"><i class="fab fa-github"></i></span></a>';
+                endif;
+                ?>
+                </div><?php
+            } 
             ?>
             </div>
         </div>
     </header>
-    <div class="entry-content content">
+    <div class="entry-content content is-large">
         <div class="container">
 		<?php
 		the_content( sprintf(
