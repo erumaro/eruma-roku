ec1c756ad15f6d34935e6627c3ec14896f5c72d6
erumaro <me@tobiasarud.se>
2022-10-12 20:43:39 +0200

    Attempt at solving generic links

--- template-parts/content-projects.php
+++ template-parts/content-projects.php
@@ -3,7 +3,7 @@
         <div class="columns">
             <div class="column">
                 <div class="project-wrapper" <?php getFeaturedImage() ?>>
-                    <a href="<?php echo esc_url( get_permalink() ) ?>">
+                    <a href="<?php echo esc_url(get_permalink()) ?>">
                         <div class="project-content">
                             <div class="project-text">
                             <?php
@@ -15,14 +15,14 @@
                     </a>
                     <?php if(get_field('live_link') || get_field('github_link')) { ?>
                     <footer class="project-footer buttons">
-                    <?php
-                    if( get_field('live_link') ) :
-                        echo '<a class="button is-primary is-inverted" href="' . esc_url( get_field('live_link') ) . '">'. __( 'Visit Website', 'eruma-roku' ) .'</a>';
-                    endif;
-                    if( get_field('github_link') ) :
-                        echo '<a class="button is-primary is-inverted" href="' . esc_url( get_field('github_link') ) . '"><span>'. __( 'Visit Github Repo', 'eruma-roku' ) .'</span><span class="icon is-medium"><i class="fab fa-github"></i></span></a>';
-                    endif;
-                    ?>
+                        <?php
+                        if(get_field('live_link') ) :
+                            echo '<a class="button is-primary is-inverted" href="' . esc_url(get_field('live_link')) . '">'. __('Visit ', 'eruma-roku') . get_the_title() .'</a>';
+                        endif;
+                        if(get_field('github_link') ) :
+                            echo '<a class="button is-primary is-inverted" href="' . esc_url(get_field('github_link')) . '"><span>'. __('Visit Github Repo', 'eruma-roku') .'</span><span class="icon is-medium"><i class="fab fa-github"></i></span></a>';
+                        endif;
+                        ?>
                     </footer>
                     <?php } ?>
                 </div>
